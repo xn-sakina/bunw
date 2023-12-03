cc98bad8ef335dee8b587d2dcd94238e1c379259
fz6m <59400654+fz6m@users.noreply.github.com>
2023-12-03 12:49:45 +0800

    chore: format

--- types/compiled.d.ts
+++ types/compiled.d.ts
@@ -2,10 +2,12 @@ declare module 'bunw/compiled/execa' {
   import * as p from 'execa';
   export = p;
 }
+
 declare module 'bunw/compiled/fs-extra' {
   import * as p from 'fs-extra';
   export = p;
 }
+
 declare module 'bunw/compiled/chalk' {
   import * as p from 'chalk';
   export = p;
